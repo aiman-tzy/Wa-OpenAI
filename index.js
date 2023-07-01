e848b0b3fca6fd855ea53db7a83067d6dbd9f78c
Lynxaiman <87105811+aiman-tzy@users.noreply.github.com>
2023-07-01 13:23:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "yusril";
-const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const donet = "lynxaiman.my.id";
+const owner = ["60102810046"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
